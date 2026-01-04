818ad48c83c29c2ad483fa4dbe037022943ea62c
jonas van parijs <jonas.vanparijs@ugent.be>
2026-01-04 21:27:36 +0100

    action

--- components/specificComponents/Person/Person.js
+++ components/specificComponents/Person/Person.js
@@ -14,33 +14,39 @@ export default class Person extends Component {
                 <div {...storyblokEditable(this.props.blok)} className={css["wrapper"]}>
                     <div className={css["content"]}>
                         <div className={[css["box"], css["head"]].join(" ")}>
-                            {/* AANGEPAST: Alleen title tonen, geen firstname/lastname meer */}
                             <h1>{this.props.blok.title}</h1>
                         </div>
                         <div className={[css["box"], css["sidebar"]].join(" ")}>
-                            {/* AANGEPAST: Vraagteken toegevoegd voor veiligheid */}
                             <div className={css["personalimage"]}><img src={this.props.blok.photo?.filename} /></div>
                             <div className={css["personaldetails"]}>
-                                {/* AANGEPAST: Alleen title */}
                                 <div className={css["personaldetailitem"]}>{this.props.blok.title}</div>
                                 <div className={css["personaldetailitem"]}>{this.props.blok.dateofbirth}</div>
-                                {/* AANGEPAST: Locatie regel is hier verwijderd */}
                             </div>
                         </div>
+                        
+                        {/* 1. Het blok voor Ervaring */}
                         <div className={[css["box"], css["experience"]].join(" ")}>
                             <h2>Experience</h2>
                             {this.props.blok.experiences?.map((nestedBlok) => (
                                 <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
                             ))}
                         </div>
+
+                        {/* 2. NIEUW: Hier zorgen we dat extra blokken (zoals je Carrousel) getoond worden */}
+                        {this.props.blok.bottom_blocks && (
+                            <div className={[css["box"], css["experience"]].join(" ")} style={{marginTop: "20px"}}>
+                                {this.props.blok.bottom_blocks.map((nestedBlok) => (
+                                    <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+                                ))}
+                            </div>
+                        )}
+
                         <div className={[css["box"], css["foot"]].join(" ")}>
-                            {/* AANGEPAST: Ook hier title gebruikt ipv firstname/lastname */}
                             <div>&copy; {this.props.blok.title} {new Date().getFullYear()}</div>
                         </div>
                     </div>
                 </div>
             </>
         );
-        
     }
 }
