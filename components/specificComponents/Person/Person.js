1056bfd777f217ac1b4ebd80ebbc18cdd5ef025a
jonas van parijs <jonas.vanparijs@ugent.be>
2026-01-05 11:57:26 +0100

    action to make teacherspage better

--- components/specificComponents/Person/Person.js
+++ components/specificComponents/Person/Person.js
@@ -1,18 +1,32 @@
 ﻿import React, { Component } from "react";
 import css from "./Person.module.scss";
-import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+// We importeren renderRichText om de Bio te kunnen lezen
+import { storyblokEditable, StoryblokComponent, renderRichText } from "@storyblok/react";
 
 export default class Person extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
-        // We maken variabelen voor de data, zodat we zeker weten dat het bestaat
         const blok = this.props.blok || {};
-        const photo = blok.photo || {};
+        
+        // --- STAP 1: DATA OPHALEN MET DE JUISTE NAMEN UIT JE LIBRARY ---
+        
+        // In je screenshot heet het plaatje 'image', dus we gebruiken blok.image
+        const image = blok.image || {};
+        
         const experiences = blok.experiences || [];
         const bottomBlocks = blok.bottom_blocks || [];
+        
+        const bio = blok.bio;
+        const dateOfBirth = blok.dateofbirth;
+        const firstName = blok.compellation; 
+        const lastName = blok.lastname;
+
+        // --- STAP 2: NAAM SAMENSTELLEN ---
+        // Als voornaam EN achternaam zijn ingevuld, plakken we ze aan elkaar.
+        // Anders gebruiken we de 'title' als backup.
+        let fullName = blok.title;
+        if (firstName && lastName) {
+            fullName = `${firstName} ${lastName}`;
+        }
 
         return (
             <div {...storyblokEditable(blok)} className={css["wrapper"]}>
@@ -20,20 +34,31 @@ export default class Person extends Component {
                     
                     {/* DE HEADER */}
                     <div className={[css["box"], css["head"]].join(" ")}>
-                        <h1>{blok.title || "Naam ontbreekt"}</h1>
+                        <h1>{fullName}</h1>
+                        
+                        {/* Datum tonen als die is ingevuld */}
+                        {dateOfBirth && (
+                            <p style={{marginTop: "10px", fontStyle: "italic"}}>
+                                Geboren op: {new Date(dateOfBirth).toLocaleDateString('nl-NL')}
+                            </p>
+                        )}
                     </div>
 
-                    {/* DE SIDEBAR (FOTO + INFO) */}
+                    {/* DE SIDEBAR (FOTO + BIO) */}
                     <div className={[css["box"], css["sidebar"]].join(" ")}>
                         <div className={css["personalimage"]}>
-                            {photo.filename && <img src={photo.filename} alt="Person" />}
+                            {/* Let op: we gebruiken hier nu 'image' i.p.v. 'photo' */}
+                            {image.filename && <img src={image.filename} alt={fullName} />}
                         </div>
-                        <div className={css["personaldetails"]}>
-                            <div className={css["personaldetailitem"]}>{blok.title}</div>
-                            <div className={css["personaldetailitem"]}>{blok.dateofbirth}</div>
-                        </div>
+
+                        {/* BIO TONEN */}
+                        {bio && (
+                            <div style={{marginTop: "20px", lineHeight: "1.6"}}>
+                                {renderRichText(bio)}
+                            </div>
+                        )}
                     </div>
-                    
+
                     {/* ERVARING */}
                     <div className={[css["box"], css["experience"]].join(" ")}>
                         <h2>Experience</h2>
@@ -42,7 +67,7 @@ export default class Person extends Component {
                         ))}
                     </div>
 
-                    {/* DE CARROUSEL (EXTRA VEILIG GEMAAKT) */}
+                    {/* CARROUSEL */}
                     {bottomBlocks.length > 0 && (
                         <div className={[css["box"], css["experience"]].join(" ")} style={{marginTop: "20px"}}>
                             {bottomBlocks.map((nestedBlok) => (
@@ -50,11 +75,12 @@ export default class Person extends Component {
                             ))}
                         </div>
                     )}
-
+                    
                     {/* FOOTER */}
                     <div className={[css["box"], css["foot"]].join(" ")}>
-                        <div>&copy; {blok.title} {new Date().getFullYear()}</div>
+                        <div>&copy; {fullName} {new Date().getFullYear()}</div>
                     </div>
+
                 </div>
             </div>
         );
