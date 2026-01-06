9c0029f716b4d80604ff8029181dced806c7ef6e
jonas van parijs <jonas.vanparijs@ugent.be>
2026-01-06 13:00:38 +0100

    action

--- components/specificComponents/FacilityPage/FacilityPage.js
+++ components/specificComponents/FacilityPage/FacilityPage.js
@@ -38,6 +38,15 @@ export default class FacilityPage extends Component {
             <div className={css["info-item"]}>
               <strong>Status:</strong> {blok.opening_status}
             </div>
+            <div className={css["info-item"]}>
+                <strong>Location:</strong> {blok.location && blok.location.map((loc, index) => (
+                    <span key={loc._uid}>
+                        {index > 0 && ", "}
+                        {loc.name}
+                    </span>
+                ))}
+                {!blok.location && "Unknown"}
+              </div>
           </div>
 
           {/* 4. BODY CONTENT (Hier komen andere blokken in te staan) */}
@@ -47,15 +56,7 @@ export default class FacilityPage extends Component {
                 <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
               ))}
           </div>
-          <div className={css["info-item"]}>
-                <strong>Location:</strong> {blok.location && blok.location.map((loc, index) => (
-                    <span key={loc._uid}>
-                        {index > 0 && ", "}
-                        {loc.name}
-                    </span>
-                ))}
-                {!blok.location && "Unknown"}
-              </div>
+          
 
         </div>
       </main>
